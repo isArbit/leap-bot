4600a0718fffec2e29e83a1a4dbfe83de8255ddc
Alien Lien <alien_lien@htc.com>
2015-09-04 16:52:27 +0800

    Minor refactor for isxxx funcs

--- leap/leap.go
+++ leap/leap.go
@@ -29,53 +29,35 @@ type Hand struct {
 // IsUpward determines if hand is moving forward.
 func (h *Hand) IsForward() bool {
 	// negative Z-axis movement
-	if h.PalmVelocity[2] < -(h.Threshold) {
-		return true
-	}
-	return false
+	return h.PalmVelocity[2] < -(h.Threshold)
 }
 
 // IsUpward determines if hand is moving back.
 func (h *Hand) IsBackward() bool {
 	// positive Z-axis movement
-	if h.PalmVelocity[2] > (h.Threshold) {
-		return true
-	}
-	return false
+	return h.PalmVelocity[2] > (h.Threshold)
 }
 
 // IsUpward determines if hand is moving up.
 func (h *Hand) IsUpward() bool {
 	// positive Y-axis movement
-	if h.PalmVelocity[1] > (h.Threshold) {
-		return true
-	}
-	return false
+	return h.PalmVelocity[1] > (h.Threshold)
 }
 
 // IsDownward determines if hand is moving down.
 func (h *Hand) IsDownward() bool {
 	// negative Y-axis movement
-	if h.PalmVelocity[1] < -(h.Threshold) {
-		return true
-	}
-	return false
+	return h.PalmVelocity[1] < -(h.Threshold)
 }
 
 // IsRight determines if hand is moving right.
 func (h *Hand) IsRight() bool {
 	// positive X-axis movement
-	if h.PalmVelocity[0] > (h.Threshold) {
-		return true
-	}
-	return false
+	return h.PalmVelocity[0] > (h.Threshold)
 }
 
 // IsLeft determines if hand is moving left.
 func (h *Hand) IsLeft() bool {
 	// negative X-axis movement
-	if h.PalmVelocity[0] < -(h.Threshold) {
-		return true
-	}
-	return false
+	return h.PalmVelocity[0] < -(h.Threshold)
 }
